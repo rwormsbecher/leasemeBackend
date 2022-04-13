3d4290a72370e16562fbb1aceb62111f0543f949
rodney22@live.nl <rodney22@live.nl>
2022-04-13 17:30:47 +0200

    make sure models are exported

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,6 @@
 from models.counterparty import *
 from models.user  import *
+from models.invoice_general import *
+from models.lease_agreement import *
+from models.invoice_supplier_detail import *
+
